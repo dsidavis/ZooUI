3a12101af347da1d1aa08e555ce5a330df8114b7
Duncan Temple Lang <duncan@r-project.org>
2018-04-27 04:54:47 -0700

    add and delete a row

--- addResult.js
+++ addResult.js
@@ -1,4 +1,79 @@
+//XXX duplicated
+var VarNames = ['country', 'year', 'virus', 'species', 'diagTest'];
+
+
 function addResult()
 {
+    var sels = {};
 
-}    
+    var n = 0, i;
+    for(i = 0; i < VarNames.length; i++) {
+	var v = VarNames[i];
+	var sel = document.getElementById(v + "_options");
+
+	var tmp = getSelectedElements(sel);
+	n = tmp.length > n ? tmp.length : n;
+	console.log("max = " + n);
+	sels[v] = tmp;
+    }
+
+    insertResult(sels, n);
+}
+
+function getSelectedElements(obj)
+{
+    var k = obj.childNodes; // children()
+    var ans = [];
+    for(var i = 0; i < k.length; i++) {
+	if(k[i].selected)
+	    ans.push(k[i].value);
+    }
+    return(ans);
+}
+
+
+var RowNum = 1;
+
+function insertResult(vals, n)
+{
+
+    var table = document.getElementById("resultsTable");
+    table = table.childNodes[1]; // tbody
+
+    // assume the varnames are arranged in order of result table
+    // Create a new tr
+    //    Fill in each column.
+    console.log("creating " + n + " <tr>s with " + VarNames.length );
+    for(var j = 0; j < n ; j++) {
+	var tr = document.createElement("tr");
+	tr.id = "RowNum_" + RowNum;
+	RowNum = RowNum + 1;
+
+	var td = document.createElement("td");	
+	td.innerHTML = '<img src=trashCan.jpg width=32 height=32 onclick=deleteRow("' +  tr.id + '")></img>';
+	tr.appendChild(td);
+//	td.childNodes[0].onclick = "deleteRow(" +  td.id + ");"
+	
+	for(var i = 0; i < VarNames.length; i++) {
+	    var v = VarNames[i];
+	    var pos = min(j, vals[v].length - 1);
+//	    console.log("td for " + j + " " + " and " + i  + " " + vals[v][pos]);
+	    td = document.createElement("td");
+	    td.innerHTML = vals[v][pos];
+	    tr.appendChild(td);
+	}
+	table.appendChild(tr);
+    }
+}
+
+function min(a, b)
+{
+    return(a < b ? a : b);
+}
+
+
+function deleteRow(id)
+{
+    var obj = document.getElementById(id);
+    obj.parentNode.removeChild(obj);
+}
