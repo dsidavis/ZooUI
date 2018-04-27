59b4b2cbddca04cd7d45287b13cecd98c740014e
Duncan Temple Lang <duncan@r-project.org>
2018-04-26 20:01:03 -0700

    fix a missing quote in ui; show that sliders don't update if one is already set to true in varThresholds

--- sliders.js
+++ sliders.js
@@ -3,11 +3,19 @@ function updateGlobalThreshold(obj)
     var els = ['country', 'year', 'virus', 'species', 'diagTest'];
     var val = obj.value;
     for(var i = 0; i < els.length; i++) {
-	var s = document.getElementById(els[i] + "_slider");
-	s.value = val;
+	if(varThresholds[els[i]] == false) {
+	    var s = document.getElementById(els[i] + "_slider");
+	    s.value = val;
+	}
     }
 }
 
+/*
+  We keep a record of the threshold values set explicitly and individually on each slider and not
+  just via the global threshold slider.
+  When  we change an individual slider directly, we change the corresponding varThreshold value to be different from -1
+*/
+var varThresholds = {year: true, country: false, virus: false, species: false, diagTest: false};
 
 function updateThreshold(obj, id)
 {
