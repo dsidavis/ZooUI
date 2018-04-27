b8fc65bb0f5d64049bf0f2aace28837b4278b202
Duncan Temple Lang <duncan@r-project.org>
2018-04-26 20:13:59 -0700

    new data for the relevant paper (no spaces in name).

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 var docInfo = {
-    title: "Isolation of Japanese Encephalitis and Getah Viruses from Mosquitoes (Diptera: Culicidae) Collected near Camp Greaves, Gyonggi Province, Republic of Korea, 2000",
-    abstract: "As part of an evaluation of the ecology of arthropod-borne diseases in the Republic of Korea (ROK), we examined 8,765 mosquitoes captured in Paju County, Gyonggi Province, ROK, for the presence of viruses. Mosquitoes were captured in propane lantern/human-baited Shannon traps, Mosquito Magnet traps, or American Biophysics Corporation (East Greenwich, RI) miniature light traps with or without supplemental octenol bait and/or dry ice. Mosquitoes were identiÞed to species, placed in pools of up to 40 mosquitoes each, and tested on Vero cells for the presence of virus. A total of 15 virus isolations were made from 293 pools of mosquitoes. Viruses were identiÞed by reverse transcriptase-polymerase chain reaction and sequencing and consisted of 14 isolations of Japanese encephalitis (JE) virus and one isolation of Getah (GET) virus. All JE isolates were from Culex tritaeniorhynchus Giles, and the isolate of GET was from Aedes vexans (Meigen). The minimum Þeld infection rate for JE in Cx. tritaeniorhynchus was 3.3 per 1,000, whereas the GET virus infection rate for Ae. vexans was 0.2 per 1,000. Isolation of JE and GET indicated that both viruses were actively circulating in northern Gyonggi Province, ROK. The lack of human cases of JE among the Korean population probably is because of an effective government-mandated vaccination program. The reason for no cases among  10,000 United States military and others that reside or train nearby is unknown, but may be related to personnel protection measures (permethrin-impregnated uniforms and use of deet repellent), adult mosquito control, mosquito selection of nonhuman hosts (unpub- lished data), and the low symptomatic to asymptomatic ratio of disease in adults.",
-    year: "2003",
+    title: "Detection of antibodies to Oropouche virus in non-human primates in Goiânia City, Goiás",
+    abstract: "Arboviruses are associated with human disease, and non-human primates (NHPs) are important primary hosts. This study shows the detection of antibodies to Oropouche virus (OROV) in NHPs either living in urban parks or acclimatized at the Wild Animal Screening Center, Goiânia city. Methods: Fifty blood samples were analyzed by hemagglutination-inhibition and neutralization assays. Results: Two monkeys (Alouatta caraya) had antibodies to OROV by both techniques. Conclusions: This is the first report demonstrating the detection of OROV antibodies in Goiás State and may represent the introduction/circulation of OROV in the region and a potential risk to the human population",
+    year: "2016",
     pdf: "../NewPDFs/Oropouche/Gibrail-2016.pdf"
 //    pdf: "../NewPDFs/Getah Virus/Turrel-2003.pdf"
 };
@@ -12,5 +12,14 @@ var ModelResults = {
     country: {
 	value: ["Ireland", "Britain", "UK"],
 	probs: [.8, .76, .2]
+    },
+    virus: { values: ["encephalitis","getah"],
+	     probs: [1, 1]
+	   },
+    diagTest: { values: ["molecular"],
+		probs: [.99]
+	      },
+    species: {
+	values: ["mosquito"]
     }
 };
