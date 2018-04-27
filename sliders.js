00fe689880015ba380fd3091e8f4745519febb4a
Duncan Temple Lang <duncan@r-project.org>
2018-04-27 04:56:20 -0700

    remove debug value of true for year slider. Set varThresholds when move a specific slider

--- sliders.js
+++ sliders.js
@@ -1,6 +1,8 @@
+var VarNames = ['country', 'year', 'virus', 'species', 'diagTest'];
+
 function updateGlobalThreshold(obj)
 {
-    var els = ['country', 'year', 'virus', 'species', 'diagTest'];
+    var els = VarNames;
     var val = obj.value;
     for(var i = 0; i < els.length; i++) {
 	if(varThresholds[els[i]] == false) {
@@ -15,9 +17,13 @@ function updateGlobalThreshold(obj)
   just via the global threshold slider.
   When  we change an individual slider directly, we change the corresponding varThreshold value to be different from -1
 */
-var varThresholds = {year: true, country: false, virus: false, species: false, diagTest: false};
+var varThresholds = {year: false, country: false, virus: false, species: false, diagTest: false};
 
 function updateThreshold(obj, id)
 {
     var el = document.getElementById(id + "_options");
+    if(obj) {
+	// console.log("updateThreshold " + id);	
+	varThresholds[id] = true;
+    }
 }
