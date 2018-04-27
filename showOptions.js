f2931867953687ed83bd4c48b5b3a47da53cbd6c
Duncan Temple Lang <duncan@r-project.org>
2018-04-27 04:56:52 -0700

    show the options for the variables

--- showOptions.js
+++ showOptions.js
@@ -1,14 +1,20 @@
 function showAllOptions(data, threshold)
 {
-
-//    var varNames = Object.keys(data);
-//    for(var i 
+    var varNames = Object.keys(data);
+    console.log(varNames + " " + varNames.length);
+    for(var i = 0; i < varNames.length; i++) {
+	var v = varNames[i];
+	// alert( data[v] );
+	console.log("adding options for " + v + " " + data[v]);
+	addOptions(v, data[v], threshold);
+    }
 
 }
 function addOptions(to, options, threshold)
 {
     var n = options['values'].length;
     var sel = document.getElementById(to + "_options");
+    console.log("[addOptions] " + to + " "  + n + " " + options['value']);
 //    alert(sel + " " + sel.id);
     for(var i = 0; i < n; i++) {
 	if(options['probs'][i] < threshold) {
